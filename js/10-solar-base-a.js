95dac901b1fa9714bba6699ba7caaf22e008c67a
whatalife1 <new.life.786.786.786@gmail.com>
2026-06-16 10:53:28 +0500

    Update js/10-solar-base-a.js

--- js/10-solar-base-a.js
+++ js/10-solar-base-a.js
@@ -4,7 +4,7 @@ const SOL_LON = 74.3436;
 const SOL_TZ  = 5;
 
 const SOLAR_DEFAULTS = {
-  panelWatts: 580, panelCount: 6, tiltDeg: 10, azimuthDeg: 200,
+  panelWatts: 580, panelCount: 9, tiltDeg: 10, azimuthDeg: 200,
   sysEff: 0.82, batteryKwh: 0, pkrPerUnit: 60, cloudPct: 0
 };
 let solarCfg = { ...SOLAR_DEFAULTS };

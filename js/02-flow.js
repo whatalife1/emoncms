8651d6e09f62beb1e1a3921b1a1093494412986b
whatalife1 <new.life.786.786.786@gmail.com>
2026-06-21 18:55:08 +0500

    Update js/02-flow.js

--- js/02-flow.js
+++ js/02-flow.js
@@ -7,7 +7,7 @@ const LAYOUT = {
  k1: { x:497, y:379, w:230, h:210, color:'#38bdf8', label:'Kenwood 1T', ly1:27, fs:34, c1:'#38bdf8', ly2:75, fs2:63, c2:'#25f447', ly3:148, fs3:24, c3:'#00c8f0', ly4:184, fs4:26, c4:'#518e35',  },
  fridge: { x:5, y:637, w:350, h:195, color:'#c084fc', label:'Fridges', ly1:19, fs:34, c1:'#38bdf8', ly2:59, fs2:62, c2:'#25f447', ly3:132, fs3:26, c3:'#00c8f0', ly4:166, fs4:25, c4:'#518d35',  },
  pc: { x:368, y:641, w:360, h:195, color:'#10b9f8', label:'PC', ly1:22, fs:52, c1:'#38bdf8', ly2:72, fs2:62, c2:'#25f447', ly3:123, fs3:28, c3:'#00c8f0', ly4:164, fs4:25, c4:'#518e35',  },
- temp: { x:9, y:590, w:236, h:40, color:'#22c55e', label:'temp', ly1:20, fs:27, c1:'#25f447',  },
+ temp: { x:253, y:588, w:236, h:40, color:'#22c55e', label:'temp', ly1:20, fs:27, c1:'#25f447',  },
  temp2: { x:501, y:598, w:228, h:35, color:'#22c55e', label:'temp2', ly1:16, fs:27, c1:'#25f447',  },
 };
 

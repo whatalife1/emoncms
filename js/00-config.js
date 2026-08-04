6acbadf372a5c68b63cd7908a5a53f05c63453d7
whatalife1 <new.life.786.786.786@gmail.com>
2026-08-04 16:38:35 +0500

    Update js/00-config.js

--- js/00-config.js
+++ js/00-config.js
@@ -1,8 +1,8 @@
 const PROXY_ENDPOINTS = [
   'https://emon-proxy.new-life-786-786-786.workers.dev',
   'https://my-vercel-proxy-1.vercel.app',
-  'https://gxmbeybitqckkonxxtcr.supabase.co/functions/v1/proxy',
-  'https://taupe-bunny-bb0a25.netlify.app/'
+  'https://gxmbeybitqckkonxxtcr.supabase.co/functions/v1/proxy'
+//  'https://taupe-bunny-bb0a25.netlify.app/'
 ];
 
 let activeProxyIndex = 0;

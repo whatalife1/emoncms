1da30c1d2d9e0ffb47daa1bbacace950c1a6a513
whatalife1 <new.life.786.786.786@gmail.com>
2026-08-06 06:44:11 +0500

    Update js/09-poll.js

--- js/09-poll.js
+++ js/09-poll.js
@@ -493,14 +493,6 @@ async function poll() {
     window.lastResultsMap = bm; 
     window.lastSolarActual = bm.get('Solar')?.value || 0;
 
-  // Run offline detection (does not block UI)
-  try {
-    const offline = await checkApplianceOffline(Math.floor(Date.now() / 1000));
-    window.applianceOfflineDetected = offline || [];
-  } catch (e) {
-    console.warn('Offline detection error:', e);
-    window.applianceOfflineDetected = [];
-  }
     renderResults(results);
     
     if (typeof checkAlerts === 'function') checkAlerts(bm);

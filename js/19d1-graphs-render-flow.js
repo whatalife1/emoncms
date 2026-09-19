8e338bc9ee85266d5acd8a9a1bd902d762baa4bb
whatalife1 <new.life.786.786.786@gmail.com>
2026-09-19 17:52:59 +0500

    Update js/19d1-graphs-render-flow.js

--- js/19d1-graphs-render-flow.js
+++ js/19d1-graphs-render-flow.js
@@ -51,11 +51,16 @@ function replayFlowDiagramAtMoment(multiData, idx, timestampSec) {
   mockMap.set('Solar V',         { value: solarW > 20 ? 380 : 0, time: timestampSec });
   mockMap.set('Solar Amps',      { value: solarW > 20 ? (solarW / 380) : 0, time: timestampSec });
   mockMap.set('Inverter Temp',   { value: 38, time: timestampSec });
-  mockMap.set('Bat V',                         { value: 52.8, time: timestampSec });
-  mockMap.set('Status',                        { value: 1, time: timestampSec });
-  mockMap.set('SOC %',                         { value: 85, time: timestampSec });
-  mockMap.set('bt_battery_charging_current',   { value: solarW > totLoad ? parseFloat(((solarW - totLoad)/52.8).toFixed(1)) : 0, time: timestampSec });
-  mockMap.set('bt_battery_discharge_current',  { value: totLoad > (solarW + gridW) ? parseFloat(((totLoad - solarW - gridW)/52.8).toFixed(1)) : 0, time: timestampSec });
+  const batSoc = getVal('bat_soc') || (multiData.find(m => m.key === 'bat_soc')?.data[idx]) || 0;
+  const batV   = getVal('bat_v')   || (multiData.find(m => m.key === 'bat_v')?.data[idx])   || 52.0;
+  const batChg = getVal('bat_chg') || (multiData.find(m => m.key === 'bat_chg')?.data[idx]) || 0;
+  const batDis = getVal('bat_dis') || (multiData.find(m => m.key === 'bat_dis')?.data[idx]) || 0;
+
+  mockMap.set('Bat V',                         { value: batV > 0 ? batV : 52.8, time: timestampSec });
+  mockMap.set('Status',                        { value: (batChg > 0.5 ? 1 : (batDis > 0.5 ? 2 : 0)), time: timestampSec });
+  mockMap.set('SOC %',                         { value: batSoc > 0 ? batSoc : 85, time: timestampSec });
+  mockMap.set('bt_battery_charging_current',   { value: batChg, time: timestampSec });
+  mockMap.set('bt_battery_discharge_current',  { value: batDis, time: timestampSec });
   mockMap.set('Water Tank',      { value: 75, time: timestampSec });
   mockMap.set('Temperature',     { value: 30, time: timestampSec });
   mockMap.set('Humidity',        { value: 55, time: timestampSec });
